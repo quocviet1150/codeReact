2c2753f070d7ed6ae53b624240d8c277e786766c
vietnq <vietnq@nissho-vn.com>
2024-03-15 13:21:38 +0700

    commit

--- src/view/instructions.js
+++ src/view/instructions.js
@@ -5,7 +5,7 @@ const Instructions = ({ isPopupVisible, togglePopup }) => {
         <>
             <div className="popup-container">
                 <div style={{ display: 'flex' }} >
-                    <h4 className="modal-title">HƯỚNG DẪN CHỌN SIZE </h4>
+                    <h4 className="modal-title">GUIDE TO SELECTING SIZE </h4>
                     <span style={{ position: 'absolute', top: '5px', right: '10px', cursor: 'pointer' }} onClick={togglePopup}>
                         X
                     </span>
@@ -13,20 +13,19 @@ const Instructions = ({ isPopupVisible, togglePopup }) => {
                 <div style={{ borderBottom: '1px solid #ccc', paddingBottom: '0%' }}></div>
 
 
-                <span style={{ fontSize: '14px' }}>Nếu bạn băn khoăn không biết chọn size nào cho phù hợp với cân nặng và chiều cao của mình, đừng lo lắng! Hãy xem bảng hướng dẫn chọn size bên dưới mà&nbsp;<span style={{ color: 'rgb(255, 0, 0)' }}>T-SHIRT</span>&nbsp;tư vấn riêng dành cho bạn</span>
+                <span style={{ fontSize: '14px' }}>If you're wondering which size to choose for your weight and height, don't worry! Please see the size selection guide below for&nbsp;<span style={{ color: 'rgb(255, 0, 0)' }}>T-SHIRT</span>&nbsp;personal advice for you</span>
 
                 <div style={{ paddingTop: '3%', paddingBottom: '4%' }}>
                     <img className="image-instruction" src={require("../image/home/introtion1.png")} alt="Shirt Store Logo" />
                     <img className="image-instruction" src={require("../image/home/introtion2.png")} alt="Shirt Store Logo" />
                 </div>
-                <img className="image-instruction" src={require("../image/home/introtion3.jpg")} alt="Shirt Store Logo" />
                 <div style={{ width: '90%', marginLeft: '5%' ,paddingBottom:'3%'}}>
-                    <span style={{ fontSize: '14px' }}>Bảng hướng dẫn chọn size trên là bảng hướng dẫn dựa trên kinh nghiệm nhiều năm của T-SHIRT theo khảo sát nhu cầu sở thích của khách hàng, tất nhiên sẽ không tuyệt đối, sẽ có những trường hợp ngoại lệ phụ thuộc theo vóc dáng, sở thích của từng người. Ví dụ có người thích mặc ôm, có người thích mặc rộng...</span>
-                    <span style={{ fontSize: '14px' }}><br></br> <br></br>Nếu bạn vẫn còn có những mắc thắc và băn khoăn cần được giải đáp? Hãy liên hệ ngay với Bộ phận Chăm sóc khách hàng của T-SHIRT qua&nbsp;Hotline <strong>(08)68 444 644</strong>&nbsp;để được hỗ trợ thêm.</span>
+                    <span style={{ fontSize: '14px' }}>The size selection guide above is a guide based on T-SHIRT's many years of experience according to a survey of customer needs and preferences. Of course, it is not absolute, there will be exceptions depending on body shape. , each person's preferences. For example, some people like to wear tight clothes, some people like to wear loose clothes...</span>
+                    <span style={{ fontSize: '14px' }}><br></br> <br></br>If you still have questions and concerns that need to be answered? Please contact T-SHIRT Customer Care Department immediately via&nbsp;Hotline <strong>(08)68 444 644</strong>&nbsp;for further support.</span>
 
                 </div>
 
-                <button onClick={togglePopup} className='btn-instruction'>Đóng</button>
+                <button onClick={togglePopup} className='btn-instruction'>Close</button>
             </div>
         </>
     );
